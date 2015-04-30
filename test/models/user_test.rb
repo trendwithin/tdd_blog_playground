05cab589011f1bae533205a4463c82fde905fbb9
Michael Becco <e4e6d4d5@gmail.com>
2015-04-30 13:11:02 -0700

    Devise User Test Pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new(email: 'me@you.com', password: 'password', role: 'admin') }
 
   it "must be valid" do
     user.must_be :valid?
