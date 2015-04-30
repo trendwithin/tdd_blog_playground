4d5dd91194d950fec12dd5b194c35f4f0a159c89
Michael Becco <e4e6d4d5@gmail.com>
2015-04-30 13:24:04 -0700

    Sign Up Link Test Pass

--- test/features/signup_test.rb
+++ test/features/signup_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
-feature "Signup" do
-  scenario "the test is sound" do
+feature "Home Page Signup Link" do
+  scenario "Visitor can sign up to blog" do
     visit root_path
-    page.must_have_content "Hello World"
+    page.must_have_content "Sign Up"
     page.wont_have_content "Goobye All!"
   end
 end
