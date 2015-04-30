96cf82af41696e53cb3decfbb316bf484e677f38
Michael Becco <e4e6d4d5@gmail.com>
2015-04-30 12:52:51 -0700

    Build Initial test_helper, add config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,15 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails"
+require "minitest/pride"
+require "minitest/rails/capybara"
+require "capybara/poltergeist"
+
+Capybara.javascript_driver = :poltergeist
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    ActiveRecord::Migration.check_pending!
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
