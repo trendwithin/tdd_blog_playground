966d00edca71895c2d0860b20a44f7ea7e7033b3
Michael Becco <e4e6d4d5@gmail.com>
2015-04-30 14:51:29 -0700

    Modify Users fixtures- add sign_in to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,3 +14,11 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# User Sign In
+def sign_in
+  visit new_user_session_path
+  fill_in "Email", with: users(:vic).email
+  fill_in "Password", with: 'password'
+  click_on "Log in"
+end
