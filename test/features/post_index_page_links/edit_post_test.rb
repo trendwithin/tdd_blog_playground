e3c74f4cf7d622be184710549262c22d4b659371
Michael Becco <e4e6d4d5@gmail.com>
2015-04-30 18:29:27 -0700

    Restructure Test Case

---
+++ test/features/post_index_page_links/edit_post_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+feature "Edit Post Link: Unavailable to Guest" do
+  scenario "Non-Logged in User Visits Post Index" do
+    visit posts_path
+    page.must_have_content "Listing Post"
+    page.wont_have_content "Edit"
+  end
+end
+
+feature "Edit Post Link: Available to Authors" do
+  scenario "Author Privelaged User Visits Post Index" do
+    sign_in_author
+    visits posts_path
+    page.must_have_content "Edit"
+  end
+end
+
+feature "Edit Post Link: Authors Limitations" do
+  scenario "Authors Can Only Edit Their Own Post" do
+    sign_in_author
+    visits posts_path
+    # situation here
+  end
+end
+
+feature "Edit Post Link: Available to Admin" do
+  scenario "Admin Visits Post Index" do
+    sign_in_admin
+    visit posts_path
+    page.must_have_content "Edit"
+  end
+end
