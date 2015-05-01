ecd76761262da36778a21547953d11abb31503cb
Michael Becco <e4e6d4d5@gmail.com>
2015-04-30 22:57:14 -0700

    Add rescure for pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   after_action :verify_authorized, unless: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perfrom this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
